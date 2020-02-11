2e6ce4b9f35b31b00562a86a735023df6afdcc14
David (edouard) Azoulai <david.azoulai@kcl.ac.uk>
2020-02-11 02:36:36 +0000

    add drug chart page; needs to make the csv file into a pdf with more infos

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, url_for
+from medications import medicationsList, generateChart
+
+from flask import Flask, render_template, request, Response, stream_with_context
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/home')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/medication', methods=['GET', 'POST'])
+def medication():
+    if request.method == 'POST':
+        selected = [med for med in medicationsList if med.name in request.form]
+        resp = Response(generateChart(selected), mimetype='text/csv')
+        resp.headers['Content-Type'] = 'application/csv'
+        resp.headers['Content-disposition'] = 'attachment;filename=drugchart.csv'
+        return resp
+
+    return render_template('medication.html', medications=medicationsList)
 
 
 if __name__ == '__main__':
