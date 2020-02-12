786b72d47308a4fe3f51cfd099d6e30a1ec5de05
Victoria Hayes <victoria.hayes@kcl.ac.uk>
2020-02-12 11:47:07 +0000

    Database Connection
    Rational: Start setting up database, with importing from flask SQL Alchemy
    Assumption: Other membes will add tables

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+hello.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(hello)
 
 
 @app.route('/')
