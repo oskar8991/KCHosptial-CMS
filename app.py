a3368def9364d96ca90ec0113c63a96619eb820d
k1892339 <ivan.hristev@kcl.ac.uk>
2020-02-13 13:53:23 +0000

    Add basic login page
    Added a new basic login form in login.html. Not functional, created using the default bootstrap classes.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
