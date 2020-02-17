6bd4cb00a42eb79ffbb12a60f5486623d097c71c
Artur <artgan06@gmail.com>
2020-02-17 14:59:23 +0000

    Update on login page
    Login page and nav bar linked

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/index')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
+
+@app.route('/login')
+def login():
+	return render_template('auth/login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
