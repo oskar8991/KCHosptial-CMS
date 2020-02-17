eff8fd3e9cf7c300534d94cfd3f3cc7a44d12a1f
Artur <artgan06@gmail.com>
2020-02-17 17:26:43 +0000

    Admin login
    You can login as admin. Session has been added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
+from flask import Flask, url_for, redirect, render_template, request, session, abort, flash
+
 app = Flask(__name__)
 
 
@@ -9,9 +11,26 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-	return render_template('auth/login.html')
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' and request.form['password'] != 'admin':
+            flash('Invalid credentials')
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+    return render_template('auth/login.html')
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.debug = True
+    app.run()
+
