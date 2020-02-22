b5879d59e1ce974d26318a169453933e5593144c
Artur <artgan06@gmail.com>
2020-02-22 03:15:38 +0000

    database setup
    The login page works with database.  There is one admin. Username: artgan  Password: admin

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, url_for, redirect, render_template, request, session, abort, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required, login_user
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #configuring database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #configuring database
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 #Creates a table for login form with id, email and password
-class Todo(db.Model):
+class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(20), nullable=False)
 	password = db.Column(db.String(20), nullable=False)
 
-
-#returning self id
-#def __repr__(self):
-#	return '<Task %r>' % self.id
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 @app.route('/home')
@@ -25,20 +26,31 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' and request.form['password'] != 'admin':
-            flash('Invalid credentials')
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('index'))
     return render_template('auth/login.html')
 
+@app.route("/logmein", methods=['POST'])
+def logmein():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.filter_by(email = username).first()
+
+    if user.password != password:
+        flash('Invalid credentials')
+        return render_template('auth/login.html') 
+    else:
+        session['logged_in'] = True
+        return redirect(url_for('index'))
+
+
+
 @app.route("/logout")
+@login_required
 def logout():
     session['logged_in'] = False
-    return index()
+    return redirect(url_for('index'))
 
 
 
