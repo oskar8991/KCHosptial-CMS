df4c1a52ef21c9c895d12ffab612a9fbe699ec7c
Artur <artgan06@gmail.com>
2020-02-24 19:51:51 +0000

    Sidebar dashboard
    Create a sidebar for dashboard

--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,27 @@
-var colors = ['#007bff','#28a745'];
-var chLine = document.getElementById("chLine");
-var chartData = {
-  labels: ["S", "M", "T", "W", "T", "F", "S"],
-  datasets: [{
-    data: [589, 445, 483, 503, 689, 692, 634],
-    borderColor: colors[0],
-    borderWidth: 4,
-    pointBackgroundColor: colors[0]
-  },
-  {
-    data: [639, 465, 493, 478, 589, 632, 674],
-    borderColor: colors[1],
-    borderWidth: 4,
-    pointBackgroundColor: colors[1]
-  }]
-};
-if (chLine) {
-  new Chart(chLine, {
-  type: 'line',
-  data: chartData,
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: false
+  var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+          datasets: [{
+            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff'
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          },
+          legend: {
+            display: false,
+          }
         }
-      }]
-    },
-    legend: {
-      display: false
-    }
-  }
-  });
-}
+      });
