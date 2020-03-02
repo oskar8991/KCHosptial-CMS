c9c29b0c7f1895228136ccb66b7ed2a049e9ecd4
Layth Mehdi <layth.mehdi@kcl.ac.uk>
2020-03-02 12:07:07 +0000

    add toggle sidebar button

---
+++ static/js/collapse.js
@@ -0,0 +1,9 @@
+	$(document).ready(function () {
+	    $('#collapse').on('click', function () {
+	        $('#sidebar').toggleClass('hidden');
+	    });
+
+	    $('#collapse').on('click', function () {
+	        $('#sideLine').toggleClass('hidden');
+	    });
+	});
