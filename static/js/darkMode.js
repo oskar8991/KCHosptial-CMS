2f8b8a3f64dc6bf0e1fecfd251cd2f17e670db92
Gosset, Antoine (k1889572) <antoine.gosset@kcl.ac.uk>
2020-04-01 21:51:25 +0100

    darkMode fix, now works on splash nb: Need to refactor

--- static/js/darkMode.js
+++ static/js/darkMode.js
@@ -1,8 +1,16 @@
 const darkMode = document.querySelector('#darkMode');
 const nav = document.querySelector('#nav');
-const navLink = document.getElementById("nav").querySelectorAll("a");
 const body = document.querySelector("body");
 const staff = document.querySelector("#staff");
+const click = document.querySelector("#click");
+const click2 = document.querySelector("#click2");
+const close = document.querySelector(".popUpHeader .close-btn");
+
+// Const specific to Splash Page
+const container = document.querySelector('.darkModeContainer');
+const splashText = document.querySelectorAll('#content');
+const icn = document.querySelectorAll('.icon');
+const h1 = document.querySelector('h1');
 
 // Const specific to Index Page
 const popUpButtonCircle = document.querySelector('.popUpButtonCircle');
@@ -37,6 +45,21 @@ window.onload = function(){
 	dark();
 };
 
+window.onclick = function(evt) {
+   if(evt.target.id == "click2" || evt.target.id == "click"){
+   		return
+   } else {
+   		closePopUp();
+   }
+   
+};
+
+function closePopUp (){
+		click.checked = false;
+		click2.checked = false;
+
+}
+
 function dark(){
 	$(function(){
 	    var test = localStorage.input === 'true'? true: false;
@@ -49,10 +72,42 @@ function dark(){
 	});
 
 
-	if (window.location.href.indexOf('index') >= 0){
-		var menubar = document.getElementById("collapse").querySelectorAll(".menubar .bar");
+	if (window.location.href.indexOf('home') >= 0){
 		if($('input[name=darkMode]').is(':checked')) {
-	        [].forEach.call(navLink, function(link) {
+			[].forEach.call(splashText, function(txtS) {
+					txtS.style.background = "#060d13";
+					txtS.style.borderColor = "#060d13";
+				});
+			[].forEach.call(icn, function(iconSplash) {
+					iconSplash.style.backgroundColor = "#1f4261";
+				});
+
+			h1.style.color = "#ebebeb";
+			container.style.backgroundColor = "#060d13";
+			container.style.borderColor = "#060d13";			
+			body.style.color = "#ebebeb";
+			body.style.backgroundColor = "#0d1a26";
+		} else {
+			[].forEach.call(splashText, function(txtS) {
+					txtS.style.background = "white";
+					txtS.style.borderColor = "#206040";
+				});
+			[].forEach.call(icn, function(iconSplash) {
+					iconSplash.style.backgroundColor = "#39ac73";
+				});
+
+			h1.style.color = "#206040";
+			container.style.backgroundColor = "#39ac73";
+			container.style.borderColor = "#206040";			
+			body.style.color = "black";
+			body.style.backgroundColor = "white";
+		}
+	} else {
+		var navLink = document.getElementById("nav").querySelectorAll("a");
+		if (window.location.href.indexOf('index') >= 0){
+			var menubar = document.getElementById("collapse").querySelectorAll(".menubar .bar");
+			if($('input[name=darkMode]').is(':checked')) {
+	        	[].forEach.call(navLink, function(link) {
 					link.style.color = "#ebebeb";
 				});
 				[].forEach.call(menubar, function(bar) {
@@ -85,7 +140,7 @@ function dark(){
 				nav.style.backgroundColor = "#060d13";
 				body.style.color = "#ebebeb";
 				body.style.backgroundColor = "#0d1a26";
-	    } else {
+	    	} else {
 	        	[].forEach.call(navLink, function(link) {
 					link.style.color = "black";
 				});
@@ -111,163 +166,169 @@ function dark(){
 		  			});
 				});
 
+				staff.style.background = "white";
 				sidebar.style.background = "#d9f2e6";
 				popUpButtonCircle.style.background = "#39ac73";
 				popUpButton.style.background = "#39ac73";
 				nav.style.backgroundColor = "#39ac73";
 				body.style.color = "black";
 				body.style.backgroundColor = "white";
-	    }
+	    	}
 
-	} else if (window.location.href.indexOf('announcements') >= 0) {
-		if($('input[name=darkMode]').is(':checked')) {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "#ebebeb";
-			});
+		} else if (window.location.href.indexOf('announcements') >= 0) {
+			if($('input[name=darkMode]').is(':checked')) {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "#ebebeb";
+				});
 
-			staff.style.background = "#060d13";
-			log.style.color = "white";
-			announcement.style.color = 'white';
-			nav.style.backgroundColor = "#060d13";
-			body.style.color = "#ebebeb";
-			body.style.backgroundColor = "#0d1a26";
-		} else {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "black";
-			});
+				staff.style.background = "#060d13";
+				log.style.color = "white";
+				announcement.style.color = 'white';
+				nav.style.backgroundColor = "#060d13";
+				body.style.color = "#ebebeb";
+				body.style.backgroundColor = "#0d1a26";
+			} else {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "black";
+				});
 
-			log.style.color = "black";
-			announcement.style.color = 'black';
-			nav.style.backgroundColor = "#39ac73";
-			body.style.color = "black";
-			body.style.backgroundColor = "white";
+				staff.style.background = "white";
+				log.style.color = "black";
+				announcement.style.color = 'black';
+				nav.style.backgroundColor = "#39ac73";
+				body.style.color = "black";
+				body.style.backgroundColor = "white";
+			}
+
+		} else if (window.location.href.indexOf('medication') >= 0) {
+			var tableHeader = document.getElementById("tableh").querySelectorAll("#tableh th");
+			if($('input[name=darkMode]').is(':checked')) {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "#ebebeb";
+				});
+				[].forEach.call(tableHeader, function(tbl) {
+					tbl.style.color = "white";
+				});
+				[].forEach.call(tableContent, function(content) {
+					content.style.color = "white";
+				});
+				[].forEach.call(chart, function(chrt) {
+					chrt.style.color = "white";
+				});
+
+				staff.style.background = "#060d13";
+				generate.style.background = "#060d13";
+				nav.style.backgroundColor = "#060d13";
+				body.style.color = "#ebebeb";
+				body.style.backgroundColor = "#0d1a26";
+			} else {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "black";
+				});
+				[].forEach.call(tableHeader, function(tbl) {
+					tbl.style.color = "black";
+				});
+				[].forEach.call(tableContent, function(content) {
+					content.style.color = "black";
+				});
+				[].forEach.call(chart, function(chrt) {
+					chrt.style.color = "black";
+				});
+
+				staff.style.background = "white";
+				generate.style.background = "#39ac73";
+				nav.style.backgroundColor = "#39ac73";
+				body.style.color = "black";
+				body.style.backgroundColor = "white";
+			}
+		} else if (window.location.href.indexOf('about') >= 0) {
+			if($('input[name=darkMode]').is(':checked')) {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "#ebebeb";
+				});
+				[].forEach.call(aboutTitle, function(abt) {
+					abt.style.background = "#060d13";
+				});
+				[].forEach.call(cardBody, function(crd) {
+					crd.style.backgroundColor = "#1f4261";
+				});
+				[].forEach.call(countour, function(ctr) {
+					ctr.style.background = "#060d13";
+				});
+
+				staff.style.background = "#060d13";
+				plz.style.color = "white";
+				nav.style.backgroundColor = "#060d13";
+				body.style.color = "#ebebeb";
+				body.style.backgroundColor = "#0d1a26";
+			} else {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "black";
+				});
+				[].forEach.call(aboutTitle, function(abt) {
+					abt.style.background = "#b3e6cc";
+				});
+				[].forEach.call(cardBody, function(crd) {
+					crd.style.backgroundColor = "white";
+				});
+				[].forEach.call(countour, function(ctr) {
+					ctr.style.background = "#b3e6cc";
+				});
+
+				staff.style.background = "white";
+				plz.style.color = "black";
+				nav.style.backgroundColor = "#39ac73";
+				body.style.color = "black";
+				body.style.backgroundColor = "white";
+			}
+
+		} else if (window.location.href.indexOf('faq') >= 0) {
+			if($('input[name=darkMode]').is(':checked')) {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "#ebebeb";
+				});
+				[].forEach.call(faqText, function(cntr) {
+					cntr.style.background = "#1f4261";
+				});
+				[].forEach.call(faqH, function(faqH) {
+					faqH.style.background = "#060d13";
+				});
+				[].forEach.call(card, function(crd) {
+					crd.style.background = "#060d13";
+				});
+				[].forEach.call(faqBtn, function(btn) {
+					btn.style.color = "white";
+				});
+
+				staff.style.background = "#060d13";
+				plz.style.color = "white";
+				nav.style.backgroundColor = "#060d13";
+				body.style.color = "#ebebeb";
+				body.style.backgroundColor = "#0d1a26";
+			}
+			else {
+				[].forEach.call(navLink, function(link) {
+					link.style.color = "black";
+				});
+				[].forEach.call(faqText, function(cntr) {
+					cntr.style.background = "#d9f2e6";
+				});
+				[].forEach.call(faqH, function(faqH) {
+					faqH.style.background = "#b3e6cc";
+				});
+				[].forEach.call(card, function(crd) {
+					crd.style.background = "#d9f2e6";
+				});
+				[].forEach.call(faqBtn, function(btn) {
+					btn.style.color = "black";
+				});
+
+				staff.style.background = "white";
+				plz.style.color = "black";
+				nav.style.backgroundColor = "#39ac73";
+				body.style.color = "black";
+				body.style.backgroundColor = "white";
+			}
 		}
-
-	} else if (window.location.href.indexOf('medication') >= 0) {
-		var tableHeader = document.getElementById("tableh").querySelectorAll("#tableh th");
-		if($('input[name=darkMode]').is(':checked')) {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "#ebebeb";
-			});
-			[].forEach.call(tableHeader, function(tbl) {
-				tbl.style.color = "white";
-			});
-			[].forEach.call(tableContent, function(content) {
-				content.style.color = "white";
-			});
-			[].forEach.call(chart, function(chrt) {
-				chrt.style.color = "white";
-			});
-
-			staff.style.background = "#060d13";
-			generate.style.background = "#060d13";
-			nav.style.backgroundColor = "#060d13";
-			body.style.color = "#ebebeb";
-			body.style.backgroundColor = "#0d1a26";
-		} else {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "black";
-			});
-			[].forEach.call(tableHeader, function(tbl) {
-				tbl.style.color = "black";
-			});
-			[].forEach.call(tableContent, function(content) {
-				content.style.color = "black";
-			});
-			[].forEach.call(chart, function(chrt) {
-				chrt.style.color = "black";
-			});
-
-			generate.style.background = "#39ac73";
-			nav.style.backgroundColor = "#39ac73";
-			body.style.color = "black";
-			body.style.backgroundColor = "white";
-		}
-	} else if (window.location.href.indexOf('about') >= 0) {
-		if($('input[name=darkMode]').is(':checked')) {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "#ebebeb";
-			});
-			[].forEach.call(aboutTitle, function(abt) {
-				abt.style.background = "#060d13";
-			});
-			[].forEach.call(cardBody, function(crd) {
-				crd.style.backgroundColor = "#1f4261";
-			});
-			[].forEach.call(countour, function(ctr) {
-				ctr.style.background = "#060d13";
-			});
-
-			staff.style.background = "#060d13";
-			plz.style.color = "white";
-			nav.style.backgroundColor = "#060d13";
-			body.style.color = "#ebebeb";
-			body.style.backgroundColor = "#0d1a26";
-		} else {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "black";
-			});
-			[].forEach.call(aboutTitle, function(abt) {
-				abt.style.background = "rgb(239, 253, 255)";
-			});
-			[].forEach.call(cardBody, function(crd) {
-				crd.style.backgroundColor = "white";
-			});
-			[].forEach.call(countour, function(ctr) {
-				ctr.style.background = "rgb(239, 253, 255)";
-			});
-
-			plz.style.color = "black";
-			nav.style.backgroundColor = "#39ac73";
-			body.style.color = "black";
-			body.style.backgroundColor = "white";
-		}
-
-	} else if (window.location.href.indexOf('faq') >= 0) {
-		if($('input[name=darkMode]').is(':checked')) {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "#ebebeb";
-			});
-			[].forEach.call(faqText, function(cntr) {
-				cntr.style.background = "#1f4261";
-			});
-			[].forEach.call(faqH, function(faqH) {
-				faqH.style.background = "#060d13";
-			});
-			[].forEach.call(card, function(crd) {
-				crd.style.background = "#060d13";
-			});
-			[].forEach.call(faqBtn, function(btn) {
-				btn.style.color = "white";
-			});
-
-			staff.style.background = "#060d13";
-			plz.style.color = "white";
-			nav.style.backgroundColor = "#060d13";
-			body.style.color = "#ebebeb";
-			body.style.backgroundColor = "#0d1a26";
-		} else {
-			[].forEach.call(navLink, function(link) {
-				link.style.color = "black";
-			});
-			[].forEach.call(faqText, function(cntr) {
-				cntr.style.background = "#d9f2e6";
-			});
-			[].forEach.call(faqH, function(faqH) {
-				faqH.style.background = "#b3e6cc";
-			});
-			[].forEach.call(card, function(crd) {
-				crd.style.background = "#d9f2e6";
-			});
-			[].forEach.call(faqBtn, function(btn) {
-				btn.style.color = "black";
-			});
-
-			plz.style.color = "black";
-			nav.style.backgroundColor = "#39ac73";
-			body.style.color = "black";
-			body.style.backgroundColor = "white";
-		}
-
 	}
 }
