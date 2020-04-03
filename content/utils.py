6a43d993bd33a647a5e157333dae8126e5d8ab98
David (edouard) Azoulai <david.azoulai@kcl.ac.uk>
2020-04-04 00:34:34 +0200

    Simplify content/utils.py

--- content/utils.py
+++ content/utils.py
@@ -1,4 +1,3 @@
-from sqlalchemy import func, and_, desc
 from collections import OrderedDict
 from app import db
 from models import Content, FlaskUsage, Glossary
@@ -8,37 +7,21 @@ def get_headings():
     '''
     Retrieves all distinct headings from database to display in edit page.
     '''
-    all_headings = (db.session
-        .query(Content.header)
-        .distinct()
-    )
-
-    headings = []
-    for heading in all_headings:
-        headings.append(heading.header)
-    return headings
+    headings = db.session.query(Content.header).distinct()
+    return [heading.header for heading in headings]
 
 def get_records():
     '''
     Retrives all records from content table.
     '''
-    all_records = (db.session
-        .query(Content.title, Content.content, Content.header, Content.page_id)
-    )
-
-
-    return all_records
+    return Content.query.all()
 
 def get_glossary():
     '''
     Retrieves a list of all the terms in the glossary.
     '''
-    words = (db.session
-        .query(Glossary.term, Glossary.description)
-    )
-
+    words = Glossary.query.all()
     initials = {word.term[0] for word in words}
-
     glossary = {
         initial: [word for word in words if word.term[0] == initial] \
         for initial in initials
