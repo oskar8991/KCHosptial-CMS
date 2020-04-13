c0f533f38e5f00df78cb7ef10123b09acb0d024e
David (edouard) Azoulai <david.azoulai@kcl.ac.uk>
2020-04-13 09:06:41 +0200

    Rename page_id to id in db (necessary for deployment).

--- content/routes.py
+++ content/routes.py
@@ -18,8 +18,8 @@ def save_record():
     content = add_img_id(content)
 
     if 'id' in data:
-        page_id = data['id']
-        article = Content.query.get(page_id)
+        id = data['id']
+        article = Content.query.get(id)
         if not article:
             return jsonify("An error occured")
 
@@ -36,9 +36,9 @@ def save_record():
 @content.route('/delete_record', methods=["GET"])
 @login_required
 def delete_record():
-    page_id = request.args.get('id')
+    id = request.args.get('id')
 
-    record = Content.query.get(page_id)
+    record = Content.query.get(id)
     if record:
         db.session.delete(record)
         db.session.commit()
@@ -65,13 +65,5 @@ def edit_record_content():
     record = Content.query.get(given_id)
     if record:
         return jsonify(record.content)
-        #return jsonify("<h1>test</h1>")
     else:
         return jsonify("error")
-    # record_content = ''
-    # for record in get_records():
-    #     if record.title == text:
-    #         record_content = record
-
-
-    # return render_template('dashboard/editor.html', content=record_content)
