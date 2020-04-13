8ea4bc83d956c15d3b9d17fba91a2d537b964029
David (edouard) Azoulai <david.azoulai@kcl.ac.uk>
2020-04-13 13:35:10 +0200

    Increase size of questions

--- src/models.py
+++ src/models.py
@@ -51,7 +51,7 @@ class Announcement(db.Model):
 
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    question_text = db.Column(db.String(64), nullable=False)
+    question_text = db.Column(db.String(128), nullable=False)
     answer = db.relationship("Answers", backref='question', cascade="delete")
     content_id = db.Column(db.Integer(), db.ForeignKey('content.id'), nullable=False)
     stat_right = db.Column(db.Integer(), default=0)
